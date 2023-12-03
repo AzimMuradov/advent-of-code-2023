978825449969ec5e818502e961a7d5202783e24a
Azim Muradov <azim.muradov.dev@gmail.com>
2023-12-03 06:35:29 +0300

    Refactor solution of day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,24 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int = input
-        .map {
-            val first = it.first(Char::isDigit)
-            val last = it.last(Char::isDigit)
+        .map { line ->
+            val first = line.first(Char::isDigit)
+            val last = line.last(Char::isDigit)
             "$first$last"
         }
         .sumOf(String::toInt)
 
     fun part2(input: List<String>): Int {
-        val digits = (1..9).map { it.toString() to it } +
-                listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-                    .mapIndexed { index, s -> s to index + 1 }
+        val wordDigits = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        val digits = buildMap {
+            for (digit in 1..9) {
+                put(key = digit.toString(), value = digit)
+            }
+            for ((index, wordDigit) in wordDigits.withIndex()) {
+                put(key = wordDigit, value = index + 1)
+            }
+        }
         return input
-            .map {
-                val first = digits.map { (str, digit) ->
-                    it.indexOf(str) to digit
-                }.filter { it.first != -1 }.minByOrNull { it.first }!!.second
-                val last = digits.map { (str, digit) ->
-                    it.lastIndexOf(str) to digit
-                }.filter { it.first != -1 }.maxByOrNull { it.first }!!.second
+            .map { line ->
+                val first = digits.getValue(line.findAnyOf(digits.keys)!!.second)
+                val last = digits.getValue(line.findLastAnyOf(digits.keys)!!.second)
                 "$first$last"
             }
             .sumOf(String::toInt)
