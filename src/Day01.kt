76508abcaabb2378f00b6bfb9a0982c04147f9c0
Azim Muradov <azim.muradov.dev@gmail.com>
2023-12-03 05:06:45 +0300

    Solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int = input
+        .map {
+            val first = it.first(Char::isDigit)
+            val last = it.last(Char::isDigit)
+            "$first$last"
+        }
+        .sumOf(String::toInt)
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digits = (1..9).map { it.toString() to it } +
+                listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+                    .mapIndexed { index, s -> s to index + 1 }
+        return input
+            .map {
+                val first = digits.map { (str, digit) ->
+                    it.indexOf(str) to digit
+                }.filter { it.first != -1 }.minByOrNull { it.first }!!.second
+                val last = digits.map { (str, digit) ->
+                    it.lastIndexOf(str) to digit
+                }.filter { it.first != -1 }.maxByOrNull { it.first }!!.second
+                "$first$last"
+            }
+            .sumOf(String::toInt)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val input = readInput("day-01-input")
 
-    val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
