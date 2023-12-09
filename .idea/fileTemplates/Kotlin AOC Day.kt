4e1aff3c1b46f80466b6aa7a15d1396fc7f909da
Azim Muradov <azim.muradov.dev@gmail.com>
2023-12-09 14:00:47 +0300

    Add AOC file template

---
+++ .idea/fileTemplates/Kotlin AOC Day.kt
@@ -0,0 +1,14 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME}
+
+#end
+#parse("File Header.java")
+fun main() {
+    fun part1(input: List<String>): Int = TODO()
+
+    fun part2(input: List<String>): Int = TODO()
+
+    val input = readInput("day-${AOC_DAY}-input")
+
+    part1(input).println()
+    part2(input).println()
+}
