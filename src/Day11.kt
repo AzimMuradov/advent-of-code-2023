f53e3649e0456acf9690e8c947a53995b61c4f4c
Azim Muradov <azim.muradov.dev@gmail.com>
2023-12-11 10:54:20 +0300

    Refactor solution of day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -4,46 +4,46 @@ import kotlin.math.abs
 typealias Space = List<List<Char>>
 
 fun main() {
-    fun Space.countEmptyLines() = map {
-        if (it.all { it == '.' }) 1 else 0
+    fun Space.countEmptyLines() = map { line ->
+        if (line.all { it == '.' }) 1 else 0
     }.runningReduce(Int::plus)
 
     fun Space.transpose(): List<List<Char>> {
         val newSpace = MutableList(first().size) { MutableList(size) { '.' } }
-
-        forEachIndexed { i, line ->
-            line.forEachIndexed { j, x ->
-                newSpace[j][i] = x
+        for ((y, line) in withIndex()) {
+            for ((x, c) in line.withIndex()) {
+                newSpace[x][y] = c
             }
         }
-
         return newSpace
     }
 
     fun calcDistSum(space: Space, expansionK: Int): Long {
-        val emptyXs = space.transpose().countEmptyLines()
-        val emptyYs = space.countEmptyLines()
-
-        val gs = buildList {
-            space.forEachIndexed { y, line ->
-                line.forEachIndexed { x, c ->
+        val galaxies = buildList {
+            for ((y, line) in space.withIndex()) {
+                for ((x, c) in line.withIndex()) {
                     if (c == '#') add(x to y)
                 }
             }
         }
 
-        val addDistK = (expansionK - 1).toLong()
+        val emptyXs = space.transpose().countEmptyLines()
+        val emptyYs = space.countEmptyLines()
+        val addDistK = expansionK - 1
 
-        return gs.flatMap { (x1, y1) ->
-            gs.map { (x2, y2) ->
-                val xD = abs(x2 - x1)
-                val xExp = abs(emptyXs[x2] - emptyXs[x1]) * addDistK
-                val yD = abs(y2 - y1)
-                val yExp = abs(emptyYs[y2] - emptyYs[y1]) * addDistK
+        return sequence {
+            for ((i, g1) in galaxies.withIndex()) {
+                val (x1, y1) = g1
+                for ((x2, y2) in galaxies.drop(i + 1)) {
+                    val xD = abs(x2 - x1)
+                    val xExp = abs(emptyXs[x2] - emptyXs[x1]) * addDistK
+                    val yD = abs(y2 - y1)
+                    val yExp = abs(emptyYs[y2] - emptyYs[y1]) * addDistK
 
-                xD + xExp + yD + yExp
+                    yield(xD + xExp + yD + yExp)
+                }
             }
-        }.reduce(Long::plus) / 2
+        }.sumOf(Int::toLong)
     }
 
     fun part1(input: List<String>): Long = calcDistSum(
